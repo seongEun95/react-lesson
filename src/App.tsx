9b5f101822fef13063f9f41c65db13542faa1881
siwon <ksw3721@gmail.com>
2023-08-16 19:36:53 +0900

    초기세팅 완료

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import { jsx, css } from '@emotion/react';
 
-function App() {
+import './style/global.css';
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div css={textCss}>emotion css</div>
+      <div css={textCss}>emotion css</div>
+      <div css={textCss}>emotion css</div>
+      <div css={textCss}>emotion css</div>
+      <div css={textCss}>emotion css</div>
+      <div css={textCss}>emotion css</div>
+      <div css={textCss}>emotion css</div>
+      <div css={textCss}>emotion css</div>
+      <div css={textCss}>emotion css</div>
     </div>
   );
 }
 
-export default App;
+const textCss = css`
+  color: blue;
+  font-size: '30px';
+`;
