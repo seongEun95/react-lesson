91b3b4c4d5bc27f1f531e447b53e1c0ef888ba48
seong eun <17adm@naver.com>
2023-10-04 21:03:19 +0900

    스타벅스 페이지 수정

--- src/page/homework/StarbucksPage.tsx
+++ src/page/homework/StarbucksPage.tsx
@@ -2,20 +2,15 @@
 /** @jsx jsx */
 import { jsx, css } from '@emotion/react';
 import { useEffect, useState } from 'react';
-import Drink from '../../components/starbucks/drink';
-
-export interface DrinkProps {
-  id: number;
-  productNameKO: string;
-  url: string;
-  isNew: string;
-}
+import Drink from '../../components/starbucks/Drink';
+import mq from '../../style/mediaQuery';
 
 export default function StarbucksPage() {
   const [drinkList, setDrinkList] = useState([]); // 데이터 빈 배열 상태
   const [loading, setLoading] = useState(true); // 데이터 불러올 때 로딩 중 상태
 
-  const getDrinkData = () => {
+  useEffect(() => {
+    // 두번째 파라미터 빈 배열, 처음 렌더링 될 때 실행
     fetch('../../../menuData.json') // 데이터 경로
       .then(res => res.json())
       .then(data => {
@@ -25,11 +20,6 @@ export default function StarbucksPage() {
       .catch(err => {
         console.dir(err);
       });
-  };
-
-  useEffect(() => {
-    // 두번째 파라미터 빈 배열, 처음 렌더링 될 때 실행
-    getDrinkData();
   }, []);
 
   return (
@@ -44,13 +34,14 @@ export default function StarbucksPage() {
       <div css={titleWrapCss}>
         <h1 css={titleCss}>콜드 브루 커피</h1>
         <p css={txtCss}>
-          디카페인 에스프레소 샷추가 가능&#40;일부 음료 제외&#41;
+          <img css={coffeeIconCss} src="/decaf.png" alt="디카페인 아이콘" />
+          디카페인 에스프레소 샷추가 가능(일부 음료 제외)
         </p>
       </div>
 
       {/* 음료 리스트 영역 */}
       <ul css={drinkListWrapCss}>
-        {drinkList.map(({ id, productNameKO, url, isNew }: DrinkProps) => {
+        {drinkList.map(({ id, productNameKO, url, isNew }) => {
           return (
             <Drink
               key={id}
@@ -66,9 +57,6 @@ export default function StarbucksPage() {
   );
 }
 
-const breakpoints = [576, 768, 991, 1200]; // 미디어 쿼리 분기점
-const mq = breakpoints.map(bp => `@media (max-width: ${bp}px)`);
-
 const titleWrapCss = css`
   display: flex;
   align-items: center;
@@ -76,7 +64,7 @@ const titleWrapCss = css`
   padding: 10px 20px;
   background-color: #f1f1f1;
 
-  ${mq[1]} {
+  ${mq.tablet} {
     align-items: flex-start;
     flex-direction: column;
     gap: 10px;
@@ -91,11 +79,14 @@ const titleCss = css`
 
 const txtCss = css`
   font-size: 14px;
-  padding: 10px 0 10px 45px;
-  background-image: url('../decaf.png');
-  background-repeat: no-repeat;
-  background-size: 40px;
-  background-position: left top 2px;
+  padding: 0px 0 0px 2px;
+`;
+
+const coffeeIconCss = css`
+  display: inline-block;
+  width: 46px;
+  margin-right: 6px;
+  vertical-align: -12px;
 `;
 
 const drinkListWrapCss = css`
@@ -105,11 +96,11 @@ const drinkListWrapCss = css`
   row-gap: 40px;
   margin: 40px 0;
 
-  ${mq[2]} {
+  ${mq.notebook} {
     grid-template-columns: repeat(3, 1fr);
   }
 
-  ${mq[1]} {
+  ${mq.tablet} {
     grid-template-columns: repeat(2, 1fr);
   }
 `;
@@ -118,11 +109,12 @@ const loadingCss = css`
   position: fixed;
   top: 0;
   left: 0;
-  bottom: 0;
-  right: 0;
+  width: 100%;
+  height: 100%;
   display: flex;
   align-items: center;
   justify-content: center;
   background-color: #00000079;
   color: #fff;
+  z-index: 10;
 `;
