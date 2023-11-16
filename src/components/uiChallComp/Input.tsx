f17fd9ff9f022951bbcb209de13f7683cb690d1c
seong eun <17adm@naver.com>
2023-11-16 18:16:06 +0900

    input 컴포넌트

---
+++ src/components/uiChallComp/Input.tsx
@@ -0,0 +1,60 @@
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import { jsx, css } from '@emotion/react';
+
+export type InputType = 'text' | 'password';
+
+interface InputProps {
+  id: string;
+  type: InputType;
+  label: string;
+  placeHolder?: string;
+  message?: string;
+  inputError: boolean;
+  handleKeyDown: (e: any) => void;
+  handleInputValueEvent: (e: any) => void;
+}
+
+export default function Input({
+  id,
+  type,
+  label,
+  placeHolder,
+  message,
+  inputError,
+  handleKeyDown,
+  handleInputValueEvent,
+}: InputProps) {
+  return (
+    <div css={inputWrapCss}>
+      <label css={labelCss} htmlFor={id}>
+        {label}
+      </label>
+      <input
+        css={inputCss(inputError)}
+        type={type}
+        id={id}
+        placeholder={placeHolder}
+        onKeyDown={handleKeyDown}
+        onChange={handleInputValueEvent}
+      />
+      <p>{message}</p>
+    </div>
+  );
+}
+
+const inputWrapCss = css`
+  margin: 10px;
+`;
+
+const labelCss = css`
+  display: block;
+  margin-bottom: 8px;
+`;
+
+const inputCss = (inputError: boolean) => css`
+  font-size: 16px;
+  padding: 7px;
+  border: 1px solid ${inputError ? 'red' : '#555'};
+  outline: none;
+`;
